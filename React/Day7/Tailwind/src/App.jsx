dc878b56cd5a0142140e8432d84a3de18f5aadaf
Mickey Singh <mikkys925@gmail.com>
2026-07-06 14:44:51 +0530

    commit

---
+++ React/Day7/Tailwind/src/App.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="bg-red-600">
+      <h1>Mickey</h1>
+
+      <div>
+        <Link>A</Link>
+        <Link>B</Link>
+        <Link>C</Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
+
